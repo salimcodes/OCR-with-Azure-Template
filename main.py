879754a1b419830f071acadd70923b962bd8b278
Salim Oyinlola <salimoyinlola@gmail.com>
2023-01-15 17:54:28 +0100

    Edited the main.py file

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello World")
+from dotenv import load_dotenv
+import os
+import time
+from PIL import Image, ImageDraw
+from matplotlib import pyplot as plt
+# Import namespaces
+
+def main():
+    global cv_client
+
+# Get Configuration Settings
+
+# Authenticate Computer Vision client
+
+# Menu for text reading functions
+
+def GetTextRead(image_file):
+    print('Reading text in {}\n'.format(image_file))
+
+
+if __name__ == "__main__":
+    main()
